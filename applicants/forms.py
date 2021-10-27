a44c2c5b4dc5489df6f625710734f78d1e377a1a
Rachel Crowley <crowleyrr@gmail.com>
2021-10-27 14:49:45 -0400

    Finalized form fields

--- applicants/forms.py
+++ applicants/forms.py
@@ -1,34 +1,18 @@
 from django import forms
 from django.core.exceptions import ValidationError
 
-MONDAY = 'MON'
-TUESDAY = 'TUE'
-WEDNESDAY = 'WED'
-THURSDAY = 'THU'
-FRIDAY = 'FRI'
-SATURDAY = 'SAT'
-SUNDAY = 'SUN'
-
-DAYS_OF_WEEK_CHOICES = [
-    (MONDAY, 'Monday'),
-    (TUESDAY, 'Tuesday'),
-    (WEDNESDAY, 'Wednesday'),
-    (THURSDAY, 'Thursday'),
-    (FRIDAY, 'Friday'),
-    (SATURDAY, 'Saturday'),
-    (SUNDAY, 'Sunday'),
-]
-
 class ApplicantForm(forms.Form):
-
-
     first_name = forms.CharField(max_length=20, required=True)
     last_name = forms.CharField(max_length=20, required=True)
     age = forms.IntegerField(required=True)
-    days_available = forms.MultipleChoiceField(required=True, choices=DAYS_OF_WEEK_CHOICES, label='Days Available to Volunteer')
+    avail_mon = forms.BooleanField(required=True)
+    avail_tues = forms.BooleanField(required=True)
+    avail_wed = forms.BooleanField()
+    avail_thurs = forms.BooleanField()
+    avail_fri = forms.BooleanField()
 
     def clean_data(self):
-        data = self.cleaned_data['first_name', 'last_name', 'age', 'days_available']
+        data = self.cleaned_data['first_name', 'last_name', 'age', 'avail_mon', 'avail_tues', 'avail_wed', 'avail_thurs', 'avail_fri']
 
         # check if age entered valid:
         if age < 15:
--- applicants/models.py
+++ applicants/models.py
@@ -2,25 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Volunteer(models.Model):
-    MONDAY = 'MON'
-    TUESDAY = 'TUE'
-    WEDNESDAY = 'WED'
-    THURSDAY = 'THU'
-    FRIDAY = 'FRI'
-    SATURDAY = 'SAT'
-    SUNDAY = 'SUN'
-
-    DAYS_OF_WEEK_CHOICES = [
-        (MONDAY, 'Monday'),
-        (TUESDAY, 'Tuesday'),
-        (WEDNESDAY, 'Wednesday'),
-        (THURSDAY, 'Thursday'),
-        (FRIDAY, 'Friday'),
-        (SATURDAY, 'Saturday'),
-        (SUNDAY, 'Sunday'),
-    ]
-
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
-    days_available = models.CharField(default=SATURDAY, max_length=30, choices=DAYS_OF_WEEK_CHOICES)
+    avail_mon = models.BooleanField(default=False)
+    avail_tues = models.BooleanField(default=False)
+    avail_wed = models.BooleanField(default=False)
+    avail_thurs = models.BooleanField(default=False)
+    avail_fri = models.BooleanField(default=False)
